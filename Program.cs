2cb72b3b4e30ad5d2c7738e03b2e8b9ffb87fb07
Daniil <d.frolenko@sh.dunice.net>
2024-05-21 10:02:04 +0300

    Add test cases to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConcatLoopBasedEncryption;
+
+Console.WriteLine(ConcatLoopBasedEncryptor.Encrypt("123456", 3));
+Console.WriteLine(ConcatLoopBasedEncryptor.Encrypt("12345", 3));
